4fcabd43aff83f94c7474df264c599dd496ba655
kar4su <karasu@ninjacods.de>
2018-10-22 18:35:33 +0200

    Initial Project add

---
+++ src/main/java/de/ninjacodes/petclinicsfg/PetclinicsfgApplication.java
@@ -0,0 +1,12 @@
+package de.ninjacodes.petclinicsfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetclinicsfgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetclinicsfgApplication.class, args);
+    }
+}
